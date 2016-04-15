2023309d10b702877173f02fb4f1c764dd0806a4
maod4github <maod4email@sina.com>
2016-04-15 11:49:13 +0800

    加入继承器工具类

--- utils/Encryptor.js
+++ utils/Encryptor.js
@@ -1,23 +1,23 @@
 var Crypto = require('crypto');
 
+// 加密器
 function Encryptor() {}
 
+// md5算法(不是标准算法),返回32个字符
 Encryptor.md5 = function (str) {
-  // md5算法 (不是标准算法) (返回32个字符) [
   var md5 = Crypto.createHash('md5');
   md5.update(str);
   return md5.digest('hex');
-  // ]
 };
 
+// 安全散列算法(Secure Hash Algorithm),返回40个字符
 Encryptor.sha1 = function (str) {
-  // 安全散列算法 (Secure Hash Algorithm) (返回40个字符) [
   var sha1 = Crypto.createHash('sha1');
   sha1.update(str);
   return sha1.digest('hex');
-  // ]
 };
 
+// 增强的加密,先sha1,再md5,返回32个字符
 Encryptor.strong = function (str) {
   return this.md5(this.sha1(str));
 };
---
+++ utils/Inheritor.js
@@ -0,0 +1,19 @@
+﻿// 继承器
+function Inheritor() {
+  if (!Object.create) {
+    Object.create = function (proto) {
+      function F() {}
+      F.prototype = proto;
+      return new F();
+    };
+  }
+}
+
+// 继承方法,childClass-子类,parentClass-父类
+// 举个例子: Inheritor.inherit(Student, Person);
+Inheritor.inherit = function (childClass, parentClass) {
+  // 这种方式是目前最佳的JS继承实现方式
+  childClass.prototype = Object.create(parentClass.prototype);
+};
+
+module.exports = Inheritor;
